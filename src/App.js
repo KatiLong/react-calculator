c59a841688511529e2210d6b92ef99f2bf8e2439
KatiLong <katileighlong@gmail.com>
2019-02-16 15:51:19 -0700

    Got lost, preparing to refactor solution with a bit of a restart

--- src/App.js
+++ src/App.js
@@ -5,42 +5,83 @@ import Total from './Total';
 
 class App extends Component {
   state = {
-    currentOperator: '',
+    current: {
+      operator: '',
+      number: -1
+    },
     total: 0,
-    buttons: ['C', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='],
-    clickFunctions: {
-      clear: () => ({ total: 0 }),
-      number: (num) => {
-        if (this.state.total === 0) return {total: num}
-        if (num === 'zero') return { total: `${this.state.total}0` }
-        else return { total: `${this.state.total}${num}` }
-      },
-      decimal: () => ({ total: `${this.state.total}.`}),
-      operator: { 
-        // add: () => ({total: parseFloat(this.state.total) + parseFloat(num)})
-      }
-    }
+    buttons: ['C', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='],  
   }
+
+  // const clickFunctions = {
+  //   clear: () => ({ total: 0 }),
+  //   number: (num) => {
+  //     if (this.state.total === 0) return {total: num}
+  //     if (num === 'zero') return { total: `${this.state.total}0` }
+  //     else return { total: `${this.state.total}${num}` }
+  //   },
+  //   decimal: () => ({ total: `${this.state.total}.`}),
+  //   operator: (type) => { 
+  //     switch (type) {
+  //       case '/' :
+  //         console.log('divide');
+  //         break;
+  //       case 'x' :
+  //         console.log('multiply');
+  //         break;
+  //       case '-' :
+  //         console.log('subtract');
+  //         break;
+  //       case '+' :
+  //         console.log('add');
+  //         break;
+  //       default: 
+  //         return { total: this.state.total}
+  //     }
+  //     // add: () => ({total: parseFloat(this.state.total) + parseFloat(num)})
+  //   }
+  // }
+  clear () { return {total: 0} }
+  number (num) {
+    console.log('number');
+    if (this.state.total === 0) return {total: num}
+    if (num === 'zero') return { total: `${this.state.total}0` }
+    else return { total: `${this.state.total}${num}` }
+  }
+  // decimal () { return { total: `${this.state.total}.`} }
+  // operator (type) {
+  //   return { total: this.state.total}
+  // }
   handleClick (e) {
     let newTotal = {total: this.state.total};
+    let newOperator = this.state.current.operator;
+    let newNumber = this.state.current.number;
     e.preventDefault();
-    console.log('Handle Click', e.target.className, newTotal);
-    // Clear
-    if (e.target.id === 'C') newTotal = this.state.clickFunctions.clear();
-    // number
-    if (e.target.className === "number") {
-      newTotal = this.state.clickFunctions.number(e.target.id);
-    }
-    // decimal
-    if (e.target.id === '.') newTotal = this.state.clickFunctions.decimal();
-    //operator
-    if (e.target.className === "operator") {
-      //where to set current operator???
-      if (this.state.currentOperator !== '') newTotal({ currentOperator: e.target.id });
-      else newTotal = this.state.clickFunctions.operator(e.target.id);
-    }
 
-    this.setState(newTotal);
+    this.setState( (state, props) => {
+      // console.log('Handle Click', e.target.className, newTotal);
+      console.log(newTotal, newOperator, newNumber, e.target);
+      // Clear
+      if (e.target.id === 'C') newTotal = this.clear();
+      // // decimal
+      // else if (e.target.id === '.') newTotal = state.clickFunctions.decimal();
+      // // number
+      else if (e.target.className === "number") {
+        newNumber = e.target.id;
+        newTotal = state.clickFunctions.number(e.target.id);
+      }
+      // //operator
+      // else if (e.target.className === "operator") {
+      //   // where to set current operator???
+      //   if (state.currentOperator !== '') newOperator = e.target.id;
+      //   else {
+      //     newTotal = this.state.clickFunctions.operator(newOperator);
+      //     newOperator = e.target.id;
+      //   }
+      // }
+      return { current: {...state.current, operator: newOperator, number: newNumber} }
+    }, () => { this.setState(newTotal) })
+    
   }
   render() {
     // .map((str, ind) => <li key={ind}>{str}</li>);
@@ -77,6 +118,22 @@ class App extends Component {
   // replaces if total 0
 // onClick Operators
 // onClick c, .
+
+// className populate idea
+// https://github.com/ahfarmer/calculator/blob/master/src/component/Button.js
+// render() {
+//   const className = [
+//     "component-button",
+//     this.props.orange ? "orange" : "",
+//     this.props.wide ? "wide" : "",
+//   ];
+
+//   return (
+//     <div className={className.join(" ").trim()}>
+//       <button onClick={this.handleClick}>{this.props.name}</button>
+//     </div>
+//   );
+
 
 
 export default App;
